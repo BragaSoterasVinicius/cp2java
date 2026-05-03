00cd91ef15aa5209b6cc5f03a1132b180673205c
BragaSoterasVinicius <viniciussoteras@gmail.com>
2026-05-03 18:41:02 -0300

    feat: initial commit

---
+++ src/main/java/br/com/fiap/_tdspo/cp2java/Cp2javaApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap._tdspo.cp2java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cp2javaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cp2javaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/_tdspo/cp2java/Cp2javaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap._tdspo.cp2java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cp2javaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
